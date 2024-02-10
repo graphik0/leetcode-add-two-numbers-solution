a54312c0f3193ede82a0e5066a55ec12bae3e7fc
GRAPHICO <18398621+graphik0@users.noreply.github.com>
2024-02-10 11:28:50 -0500

    remove Submodule

---
+++ runitonterminal/main.cpp
@@ -0,0 +1,27 @@
+#include "ListNode.h"
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* resultHead = new ListNode(0);
+        ListNode* current = resultHead;
+        int carry = 0;
+        
+        while (l1 || l2 || carry) {
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
+        }
+        
+        return resultHead->next;
+    }
+};
+
+
+int main() {
+    
+    return 0;
+}
